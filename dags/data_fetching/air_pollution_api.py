437a9ac7ad98c8913dc2a39bf18de10ad7649a99
ignsz <kaignacy@gmail.com>
2024-01-10 15:02:01 +0100

    simplify apis interface fix weather api

--- dags/data_fetching/air_pollution_api.py
+++ dags/data_fetching/air_pollution_api.py
@@ -1,20 +1,6 @@
-from typing import List
-
-from model import Location, AirAlerts
-from constants import API_URL_POLLUTION
-from utils import request_to_openweathermap
 import requests
 
 
 def is_air_quality_alert(response: requests.Response, min_healthy_aqi=3):
     records_limited = response.json()["list"][:24]
-    return any(r["main"]["aqi"] < min_healthy_aqi for r in records_limited)
-
-
-def get_air_alerts(locations: List[Location]) -> AirAlerts:
-    alerts = {}
-    for location in locations:
-        r = request_to_openweathermap(API_URL_POLLUTION, location)
-        if r.status_code == 200:
-            alerts[location] = is_air_quality_alert(r)
-    return alerts
+    return any(r["main"]["aqi"] > min_healthy_aqi for r in records_limited)
--- dags/data_fetching/weather_api.py
+++ dags/data_fetching/weather_api.py
@@ -1,22 +1,11 @@
 import requests
 
-from typing import List, Dict
-
-from model import Location, WeatherAlert
-from utils import request_to_openweathermap
-from constants import API_URL_WEATHER
+from model import WeatherAlert
 
 
-def to_weather_alert(response: requests.Response, wind_threshold:int=30):
+def to_weather_alert(response: requests.Response, wind_threshold: int=30):
     records = response.json()["list"]
-    return WeatherAlert(is_rain_alert=any(r["weather"][0]['main'] == "Rain" for r in records),
-                        is_wind_alert=any(r["wind"]['speed'] > wind_threshold for r in records))
+    return (any(r["weather"][0]['main'] == "Rain" for r in records) or
+            any(r["wind"]['speed'] > wind_threshold for r in records))
 
 
-def get_weather_alerts(locations: List[Location]) -> Dict[Location, WeatherAlert]:
-    alerts = {}
-    for location in locations:
-        response = request_to_openweathermap(API_URL_WEATHER, location)
-        if response.status_code == 200:
-            alerts[location] = to_weather_alert(response)
-    return alerts
