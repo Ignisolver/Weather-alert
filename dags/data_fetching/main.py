cae4d37243bad9c962baef1b1e634db51d37fb9c
ignsz <kaignacy@gmail.com>
2024-01-10 15:00:40 +0100

    update main

--- dags/data_fetching/main.py
+++ dags/data_fetching/main.py
@@ -1,15 +1,13 @@
-from air_pollution_api import get_air_alerts
+from dags.data_fetching.constants import AIR_ALARM, WEATHER_ALARM
+from dags.data_fetching.utils import print_alerts, get_alerts
 from sheets_api import get_user_locations
-from utils import create_alerts_table
-from weather_api import get_weather_alerts
 
 
 def main():
     locations = get_user_locations()
-    air_alerts = get_air_alerts(locations)
-    weather_alerts = get_weather_alerts(locations)
-    table = create_alerts_table(air_alerts, weather_alerts)
-    print(*table, sep='\n')
+    air_alerts = get_alerts(locations, AIR_ALARM)
+    weather_alerts = get_alerts(locations, WEATHER_ALARM)
+    print_alerts(weather_alerts, air_alerts)
 
 
 if __name__ == '__main__':
