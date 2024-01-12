9aca9ef85ea8ccf6bac1392bfc16b886dc6fd519
rynkow <rynkowski.sz@gmail.com>
2024-01-13 00:12:47 +0100

    save/lad df from s3

--- dags/data_fetching/utils.py
+++ dags/data_fetching/utils.py
@@ -1,7 +1,9 @@
 import boto3
 import requests
-from pandas import DataFrame
+import pandas as pd
 
+from pandas import DataFrame
+from io import BytesIO
 from constants import API_KEY_OPENWEATHERMAP
 from dags.data_fetching.air_pollution_api import is_air_quality_alert
 from dags.data_fetching.constants import AIR_ALARM, WEATHER_ALARM, API_URL_WEATHER, API_URL_POLLUTION, LOCATION_LON, \
@@ -32,9 +34,22 @@ def save_to_s3(
     )
 
 
-def read_from_s3(filename: str) -> DataFrame:
-    pass
-
+def read_from_s3(
+    bucket,
+    key,
+    region_name=None,
+    aws_access_key_id=None,
+    aws_secret_access_key=None,
+    aws_session_token=None,
+) -> DataFrame:
+    client = boto3.client(
+        's3',
+        region_name=region_name,
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        aws_session_token=aws_session_token,
+    )
+    return client.get_object(Bucket=bucket, Key=key)
 
 def request_to_openweathermap(url: str, location_lat, location_lon):
     r = requests.get(url, params={"lat": location_lat, "lon": location_lon, "appid": API_KEY_OPENWEATHERMAP, 'cnt':'8'})
@@ -61,8 +76,12 @@ def create_alerts_table(air_alerts: AirAlerts, weather_alerts: WeatherAlerts):
 
 def write_df_to_s3(df, filename, **kwargs):
     csv_string = df.to_csv(index=False)
-    save_to_s3(data=csv_string.getvalue(), key=filename, **kwargs)
+    save_to_s3(bucket=bucket, data=csv_string, key=filename, **kwargs)
 
+def read_df_from_s3(bucket, filename, **kwargs):
+    response = read_from_s3(bucket=bucket, key=filename)
+    data = response['Body'].read()
+    return pd.read_csv(BytesIO(data))
 
 def print_alerts(weather_alerts, air_alerts):
     weather_alerts[AIR_ALARM] = air_alerts[AIR_ALARM]
